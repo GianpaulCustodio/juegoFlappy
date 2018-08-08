6e893c07a4c04dc55ddac0388cc9b337fb2556b2
Gpspidey19 <gianpaulcustodio1198@gmail.com>
2018-08-08 14:35:32 -0500

    Primer cambio

---
+++ Flap/Flap/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Flap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.tuboArriba = new System.Windows.Forms.PictureBox();
+            this.tuboAbajo = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.puntaje = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tuboArriba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tuboAbajo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::Flap.Properties.Resources.flappy;
+            this.player.Location = new System.Drawing.Point(28, 212);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(98, 83);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // tuboArriba
+            // 
+            this.tuboArriba.BackColor = System.Drawing.Color.Transparent;
+            this.tuboArriba.Image = global::Flap.Properties.Resources.abajo;
+            this.tuboArriba.Location = new System.Drawing.Point(306, -2);
+            this.tuboArriba.Name = "tuboArriba";
+            this.tuboArriba.Size = new System.Drawing.Size(100, 262);
+            this.tuboArriba.TabIndex = 1;
+            this.tuboArriba.TabStop = false;
+            // 
+            // tuboAbajo
+            // 
+            this.tuboAbajo.BackColor = System.Drawing.Color.Transparent;
+            this.tuboAbajo.Image = global::Flap.Properties.Resources.arriba;
+            this.tuboAbajo.Location = new System.Drawing.Point(306, 318);
+            this.tuboAbajo.Name = "tuboAbajo";
+            this.tuboAbajo.Size = new System.Drawing.Size(100, 193);
+            this.tuboAbajo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tuboAbajo.TabIndex = 2;
+            this.tuboAbajo.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // puntaje
+            // 
+            this.puntaje.AutoSize = true;
+            this.puntaje.BackColor = System.Drawing.Color.Transparent;
+            this.puntaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puntaje.Location = new System.Drawing.Point(51, 212);
+            this.puntaje.Name = "puntaje";
+            this.puntaje.Size = new System.Drawing.Size(27, 29);
+            this.puntaje.TabIndex = 3;
+            this.puntaje.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Flap.Properties.Resources.fondo;
+            this.ClientSize = new System.Drawing.Size(444, 510);
+            this.Controls.Add(this.puntaje);
+            this.Controls.Add(this.tuboAbajo);
+            this.Controls.Add(this.tuboArriba);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tuboArriba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tuboAbajo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox tuboArriba;
+        private System.Windows.Forms.PictureBox tuboAbajo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label puntaje;
+    }
+}
+
---
+++ Flap/Flap/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flap
+{
+    public partial class Form1 : Form
+    {
+        int contadorMovimientos = 1;
+        bool volarArriba = false;
+        int distancia = 0;
+        Random posicionRandom = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+        public void IniciarJuego()
+        {
+            player.Location = new Point(19, 225);
+            distancia = posicionRandom.Next(-160, 118);
+            tuboArriba.Location = new Point(270, -173 - distancia);
+            tuboAbajo.Location = new Point(270, 319 - distancia);
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int cantidadMovimientos = 5;
+            if (contadorMovimientos <= cantidadMovimientos)
+            {
+                player.Image = Properties.Resources.flappy;
+                contadorMovimientos++;
+            }
+            if((contadorMovimientos > cantidadMovimientos /2)&&(contadorMovimientos <= cantidadMovimientos *2))
+            {
+                player.Image = Properties.Resources.flappy;
+                contadorMovimientos++;
+            }
+            contadorMovimientos = (contadorMovimientos > cantidadMovimientos * 2) ? 0 : contadorMovimientos;
+            int ly = player.Location.Y;
+            int lx = player.Location.X;
+
+            if(volarArriba)
+            {
+                ly = ly - 15;
+                volarArriba = false;
+            }
+            else
+            {
+                ly++;
+            }
+            player.Location = new Point(player.Location.X, ly);
+            if(player.Bounds.IntersectsWith(tuboAbajo.Bounds)|| player.Bounds.IntersectsWith(tuboArriba.Bounds))
+            {
+                IniciarJuego();
+            }
+            puntaje.Location = new Point(player.Location.X + 30, player.Location.Y - 25);
+            puntaje.Text = (tuboArriba.Location.X == player.Location.X) ? Convert.ToString((Convert.ToInt32(puntaje.Text) + 1)).ToString() : puntaje.Text;
+            
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == Convert.ToChar(Keys.Space))
+                volarArriba = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(tuboAbajo.Location.X > -140)
+            {
+                tuboAbajo.Location = new Point((tuboAbajo.Location.X) - 1, tuboAbajo.Location.Y);
+                tuboArriba.Location = new Point((tuboArriba.Location.X) - 1, tuboArriba.Location.Y);
+            }
+            else
+            {
+                distancia = posicionRandom.Next(-170, 118);
+                tuboAbajo.Location = new Point(400, 319 + distancia);
+                tuboArriba.Location = new Point(400, -173 + distancia);
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
